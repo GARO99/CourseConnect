50bfcfd2fac80f9ef34a60dddb525969bfd00aab
ArDavid14 <dcubillos904@gmail.com>
2024-12-04 02:28:15 -0500

    prueba

--- App/View/Home/Crear_Asig.php
+++ App/View/Home/Crear_Asig.php
@@ -53,10 +53,9 @@
                                           <div class="trans">
                                             <div class="btn-toolbar">
                                               <div class="btn-group btn-group">
-                                                <button type="button" class="btn btn-success" style="width: 50px; border-radius: 3px; margin:3px ; background-color:transparent; border-color:green" onclick="window.location='<?php echo ($data[5]=='id' || $data[5]=='name')? RUTURL.'c_product/'.$data[2].'/'.$row->idcategoria.'/view/'.$data[6].'/'.$data[7].'/'.$data[5]: RUTURL.'c_product/'.$data[2].'/'.$row->idcategoria.'/view'; ?>'"><i class="fa fa-fw fa-eye text-green"></i></button>
-                                                <button type="button" class="btn btn-success" style="width: 50px; border-radius: 3px; margin: 3px; background-color:transparent; border-color:#f39c12" onclick="window.location='<?php echo ($data[5]=='id' || $data[5]=='name')? RUTURL.'c_product/'.$data[2].'/'.$row->idcategoria.'/update/'.$data[6].'/'.$data[7].'/'.$data[5]: RUTURL.'c_product/'.$data[2].'/'.$row->idcategoria.'/update'; ?>'"><i class="fa fa-fw fa-pencil-square-o text-yellow"></i></button>
-                                                <button type="button" class="btn btn-success" style="width: 50px; border-radius: 3px; margin: 3px; background-color:transparent; border-color:red" onclick="window.location='<?php echo ($data[5]=='id' || $data[5]=='name')? RUTURL.'c_product/'.$data[2].'/'.$row->idcategoria.'/delete/'.$data[6].'/'.$data[7].'/'.$data[5]: RUTURL.'c_product/'.$data[2].'/'.$row->idcategoria.'/delete'; ?>'"><i class="fa fa-trash text-red"></i></button>
-                                              </div>
+                                                <button type="button" class="btn btn-success" style="width: 50px; border-radius: 3px; margin:3px ; background-color:transparent; border-color:green" onclick="window.location=''"><i class="fa fa-fw fa-eye text-green"></i></button>
+                                                <button type="button" class="btn btn-success" style="width: 50px; border-radius: 3px; margin: 3px; background-color:transparent; border-color:#f39c12" onclick="window.location=''"><i class="fa fa-fw fa-pencil-square-o text-yellow"></i></button>
+                                                <button type="button" class="btn btn-success" style="width: 50px; border-radius: 3px; margin: 3px; background-color:transparent; border-color:#f39c12" onclick="window.location=''"><i class="fa fa-fw fa-pencil-square-o text-yellow"></i></button>
                                             </div>
                                           </div>
                                         </td>
